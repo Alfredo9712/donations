7396159b22404e4c2746a85d876e348a27a1fb74
Alfredo Mora <alfredomora@Alfredos-Air.lan1>
2022-11-05 15:06:48 -0700

    make schema fields required

--- pages/api/public/users.ts
+++ pages/api/public/users.ts
@@ -2,6 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
 import dbConnect from "../../../src/middleware/dbConnect";
 import User from "../../../src/models/userModel";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
 const handler = nc()
   .use(async (req: NextApiRequest, res: NextApiResponse, next) => {
     await dbConnect();
@@ -11,12 +16,19 @@ const handler = nc()
   // @route     post /api/public/users
   // @access    Public
   .post(async (req: NextApiRequest, res: NextApiResponse) => {
-    const { email } = req.body;
+    const { name, email, password } = req.body;
     const user = await User.findOne({ email });
+
     try {
       if (user) return res.status(200).json({ message: "user already exists" });
 
-      const newUser = await User.create(req.body);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const newUser = await User.create({
+        email,
+        name,
+        password: hashedPassword,
+      });
 
       return res.status(200).json(newUser);
     } catch (error) {
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,15 +1,18 @@
-import mongoose, { Mongoose, Schema } from "mongoose";
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
     unique: true,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
 });
 
