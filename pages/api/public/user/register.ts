6047563ba581acfa01f73c225a2a6a9e58f41f76
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2022-11-05 15:36:43 -0700

    complete user sign up

--- pages/api/public/user/register.ts
+++ pages/api/public/user/register.ts
@@ -4,6 +4,7 @@ import dbConnect from "../../../../src/middleware/dbConnect";
 import User from "../../../../src/models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
@@ -39,7 +40,15 @@ const handler = nc()
         { expiresIn: Math.floor(Date.now() / 1000) + 60 * 60 }
       );
 
-      return res.status(200).json({ token });
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("AUTH_TOKEN", token, {
+          httpOnly: true,
+          maxAge: Math.floor(Date.now() / 1000) + 60 * 60,
+        })
+      );
+
+      return res.status(200).json({ newUser });
     } catch (error) {
       return res.status(400).json(error);
     }
