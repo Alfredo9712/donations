a3afa5ac190b665d7c2abe9c92efd0d3c826d6be
Alfredo Mora <alfredomora@Alfredos-Air.attlocal.net>
2022-12-03 18:48:39 -0800

    add temp sign up page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthContext } from "../src/hooks/useAuthContext";
 import { Input, Box, Flex } from "@chakra-ui/react";
 
@@ -6,7 +6,9 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const { dispatch } = useAuthContext();
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
 
+  console.log(user);
   return (
     <div className={styles.container}>
       <h1>Donations</h1>
@@ -14,10 +16,38 @@ export default function Home() {
       <Box>
         <Flex flexDirection={"column"}>
           <Box>
-            <Input />
+            <Input
+              placeholder="Name"
+              onChange={(e) =>
+                setUser((prevState) => ({
+                  ...prevState,
+                  name: e.target.value,
+                }))
+              }
+            />
           </Box>
           <Box>
-            <Input />
+            <Input
+              placeholder="Email"
+              onChange={(e) =>
+                setUser((prevState) => ({
+                  ...prevState,
+                  email: e.target.value,
+                }))
+              }
+            />
+          </Box>
+          <Box>
+            <Input
+              placeholder="Password"
+              type={"password"}
+              onChange={(e) =>
+                setUser((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+            />
           </Box>
         </Flex>
       </Box>
