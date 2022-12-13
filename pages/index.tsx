ef8e5a378a027896d0500194722f6c037dfee990
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2022-12-12 20:51:26 -0800

    start navbar styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, UnorderedList, ListItem } from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
+
 import { useAuthContext } from "../src/hooks/useAuthContext";
+
+const navItems = ["Login"];
 
 export default function Home() {
   const { user, isLoading } = useAuthContext();
 
-  return <Box>Home</Box>;
+  return (
+    <Box>
+      <Flex alignItems={"center"} justifyContent="space-between">
+        <Box>
+          <Image
+            src="/images/donations.png"
+            alt="Donation logo"
+            width={70}
+            height={70}
+          />
+        </Box>
+
+        <Box>
+          <UnorderedList
+            display="flex"
+            listStyleType={"none"}
+            justifyContent="space-between"
+          >
+            <ListItem>Home</ListItem>
+            <ListItem marginX={3}>Login</ListItem>
+            <ListItem>Logout</ListItem>
+          </UnorderedList>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
